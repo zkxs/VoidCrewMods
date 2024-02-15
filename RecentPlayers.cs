e97b915ca8e41f528e59021205e0cbc3cc103005
Michael Ripley <zkxs00@gmail.com>
2024-02-14 19:23:51 -0600

    Don't touch Photon until Void Crew initializes it

--- RecentPlayers.cs
+++ RecentPlayers.cs
@@ -3,8 +3,10 @@
 // Copyright © 2024 Michael Ripley
 
 using System;
+using System.Threading;
 using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 using Photon.Pun;
 using Photon.Realtime;
 using Steamworks;
@@ -20,17 +22,19 @@ namespace RecentPlayers
 
         internal static new ManualLogSource? Logger;
 
+        private static int initialized = 0;
+
         private void Awake()
         {
             try
             {
                 Logger = base.Logger; // this lets us access the logger from static contexts later: namely our patches.
 
-                PhotonNetwork.AddCallbackTarget(new PhotonInRoomCallbacks());
-                PhotonNetwork.AddCallbackTarget(new PhotonMatchmakingCallbacks());
+                Harmony harmony = new Harmony(GUID);
+                harmony.PatchAll();
 
                 // Nothing broke. Emit a log to indicate this.
-                Logger.LogInfo($"{MOD_NAME} successfully initialized!");
+                Logger.LogInfo($"{MOD_NAME} successfully installed patches!");
             }
             catch (Exception e)
             {
@@ -46,6 +50,7 @@ namespace RecentPlayers
                 if (!player.IsLocal)
                 {
                     SteamFriends.SetPlayedWith(GetSteamID(player));
+                    Logger!.LogDebug($"SetPlayedWith({player})");
                 }
             }
             catch (ArgumentNullException)
@@ -65,6 +70,28 @@ namespace RecentPlayers
         private static CSteamID GetSteamID(Player player)
         {
             return new CSteamID(ulong.Parse((string)player.CustomProperties["STEAMID"]));
+        }
+
+        [HarmonyPatch]
+        public class HarmonyPatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(MatchmakingHandler), nameof(MatchmakingHandler.StartRetrievingRooms))]
+            public static void RegisterPhotonCallbacks(LoadBalancingClient ___client)
+            {
+                // atomically set `initialized` to 1 and return the previous value. This will only return `0` once.
+                if (Interlocked.CompareExchange(ref initialized, 1, 0) == 0)
+                {
+                    // this code will only run once, even when called by multiple threads
+                    ___client.AddCallbackTarget(new PhotonInRoomCallbacks());
+                    ___client.AddCallbackTarget(new PhotonMatchmakingCallbacks());
+                    Logger!.LogInfo($"{MOD_NAME} successfully finished initializing!");
+                }
+                else
+                {
+                    Logger!.LogDebug("MatchmakingHandler.StartRetrievingRooms() was called again!");
+                }
+            }
         }
     }
 }
